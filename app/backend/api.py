5b2a745a9e6b7403fff7513155f527333b029497
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-01-14 12:38:50 +0530

    git commit -m "Fixed API endpoint to handle user authentication and improved error handling in backend/api.py"

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
+from typing import List
+
+from app.core.ai_agent import get_response_from_ai_agent
+
+from app.config.settings import settings
+from app.common.logger import get_logger
+
+logger = get_logger(__name__)
+
+app = FastAPI(title = "Multi-AI Agent")
+
+class RequestState(BaseModel):
+    model_name:str
+    system_prompt:str
+    messages:List[str]
+    allow_search: bool
+
+@app.post("/chat")
+def chat_endpoint(request:RequestState):
+    
