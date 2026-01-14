4c07591a724296f78ec6d8b53926ccea15379cc2
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-01-14 18:55:00 +0530

    git commit -m "streamlit Ui"

---
+++ app/frontend/ui.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import requests
+
+from app.config.settings import settings
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+st.set_page_config(page_title="Multi Ai Agent", layout="centered")
+
+system_prompt = st.text_area("Define your Ai Agent:" , height=70)
+selected_model = st.selectbox("Select your Ai model:" , settings.ALLOWED_MODEL_NAMES)
+
+allow_web_search = st.checkbox("Allow Web Search")
+
+user_query = st.text_area("Enter your query:", height=150)
+
+API_URL = "http://127.0.0.1.9999/chat"
+
+if st.button("Ask Agent") and user_query.strip():
+    
+    payload = {
+        "model_name": selected_model,
+        "system_prompt": system_prompt,
+        "messages": [user_query],
+        "allow_search": allow_web_search
+    }
+
+    try:
+        logger.info("sending request to backend")
+
+        response = requests.post(API_URL, json=payload)
+
+        if response.status_code==200:
+            agent_response = response.json().get("response","")
+            logger.info("Sucessfully recived response from backend")
+
+            st.subheader("Agent Response")
+            st.markdown(agent_response.replace("\n","<br>"), unsafe_allow_html=True)
+
+        else:
+            logger.error("Backend errror")
+            st.error("Error with backend")
+
+    except Exception as e:
+        logger.error("Error occured while sending request to backend")
+        st.error(str(CustomException("Failde to communicate to backend")))
