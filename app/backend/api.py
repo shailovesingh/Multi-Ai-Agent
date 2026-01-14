3d9312d8df883a6cac8bf50468d18d99a4841291
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-01-14 18:26:13 +0530

    git commit -m "Added CustomException handling in API module"

--- app/backend/api.py
+++ app/backend/api.py
@@ -7,6 +7,8 @@ from app.core.ai_agent import get_response_from_ai_agent
 
 from app.config.settings import settings
 from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
 
 logger = get_logger(__name__)
 
@@ -20,4 +22,26 @@ class RequestState(BaseModel):
 
 @app.post("/chat")
 def chat_endpoint(request:RequestState):
+    logger.info(f"Received request for model: {request.model_name}")
+
+    if request.model_name not in settings.ALLOWED_MODEL_NAMES:
+        logger.warning("Invalid model name")
+        raise HTTPException(status_code=400, detail="Invalid model name")
     
+    try:
+        response = get_response_from_ai_agent(
+            request.model_name,
+            request.messages,
+            request.allow_search,
+            request.system_prompt
+        )
+
+        logger.info(f"Successfully got response from AI Agent {request.model_name}")
+
+        return {"response": response}
+    
+    except Exception as e:
+        logger.error("some error occurrded during response generation")
+        raise HTTPException(status_code=500, detail=str(CustomException("Failed to get Ai response", e)))
+    
+
