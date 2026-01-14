2101ac9f658edf57d8d94c2e0da1dbb6b00f7a96
Shailove <124027365+shailovesingh@users.noreply.github.com>
2026-01-14 21:32:27 +0530

    git commit -m "changes in backend"

--- app/backend/api.py
+++ app/backend/api.py
@@ -1,18 +1,14 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
-
 from typing import List
-
-from app.core.ai_agent import get_response_from_ai_agent
-
+from app.core.ai_agent import get_response_from_ai_agents
 from app.config.settings import settings
 from app.common.logger import get_logger
 from app.common.custom_exception import CustomException
 
-
 logger = get_logger(__name__)
 
-app = FastAPI(title = "Multi-AI Agent")
+app = FastAPI(title="MULTI AI AGENT")
 
 class RequestState(BaseModel):
     model_name:str
@@ -22,26 +18,30 @@ class RequestState(BaseModel):
 
 @app.post("/chat")
 def chat_endpoint(request:RequestState):
-    logger.info(f"Received request for model: {request.model_name}")
+    logger.info(f"Received request for model : {request.model_name}")
 
     if request.model_name not in settings.ALLOWED_MODEL_NAMES:
         logger.warning("Invalid model name")
-        raise HTTPException(status_code=400, detail="Invalid model name")
+        raise HTTPException(status_code=400 , detail="Invalid model name")
     
     try:
-        response = get_response_from_ai_agent(
+        response = get_response_from_ai_agents(
             request.model_name,
             request.messages,
             request.allow_search,
             request.system_prompt
         )
 
-        logger.info(f"Successfully got response from AI Agent {request.model_name}")
+        logger.info(f"Sucesfully got response from AI Agent {request.model_name}")
 
-        return {"response": response}
+        return {"response" : response}
     
     except Exception as e:
-        logger.error("some error occurrded during response generation")
-        raise HTTPException(status_code=500, detail=str(CustomException("Failed to get Ai response", error_detail=e)))
+        logger.error("Some error ocuured during reponse generation")
+        raise HTTPException(
+            status_code=500 , 
+            detail=str(CustomException("Failed to get AI response" , error_detail=e))
+            )
     
+
 
